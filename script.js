1a83cfeceb6f8edfee829488a7ed8300db4e6d42
MykeNuLeng <linford.mike@gmail.com>
2021-05-23 23:03:22 +0100

    Animated the site using scrollmagic and timeline. Altered style.css to make it look more natural, added a shadow to the content box.

---
+++ script.js
@@ -0,0 +1,19 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to(".slope", 10, { y: -500 })
+  .fromTo(".jump", { y: -50 }, { y: -300, duration: 10 }, "-=10")
+  .fromTo(".bg", { y: -20 }, { y: 0, duration: 10 }, "-=10")
+  .to(".content", 10, { top: "0%" }, "-=10")
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "150%",
+  triggerHook: 0
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
